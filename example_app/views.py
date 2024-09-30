a14533f1941ebb6d1f0a457c7c4d5bfc7d80c1cb
Mr.back <bjh59460@gmail.com>
2024-09-30 20:01:08 +0900

    first week

---
+++ example_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+   return HttpResponse("Hello, World!")
+
+def home(request):
+    return HttpResponse("Welcome to the Example App Home Page!")
+
+
+def about(request):
+   return HttpResponse("This is the About Page for the Example App.")
+
+
+def contact(request):
+   return HttpResponse("Contact us at: contact@example.com")
